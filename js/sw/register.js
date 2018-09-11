1b35c77a976491813c082fedaf2ee2a1326049f7
Vinícius Brito <vinicius.fernandes.brito@gmail.com>
2018-09-11 13:09:26 -0300

    Update test

--- js/sw/register.js
+++ js/sw/register.js
@@ -1,3 +1,5 @@
 if('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./sw.js')
+        .then(reg => console.log('SW Registred'))
+        .catch(err => console.log('Error:', err))
 }
